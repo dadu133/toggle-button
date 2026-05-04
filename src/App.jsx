ee3c4ad61e4833163e447916c2a959e59db50209
dadu133 <harshsharma5815@gmail.com>
2026-05-04 16:39:22 +0530

    final commit and pushing the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,68 @@
 import { useState, useRef, useEffect, use } from 'react';
 export default function App() {
   const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchpokemon, setSearchpokemon] = useState("");
+  const fetchPokemon = async () => {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=130");
+      const data = await response.json();
+      const pokemondata = await Promise.all(data.results.map(async (pokemon) => {
+        const res = await fetch(pokemon.url);
+        return res.json();
+      }));
+      console.log(pokemondata[0].sprites.front_default);
+      setPokemon(pokemondata);
+    } catch (err) {
+      console.error("Error fetching pokemon data:", err);
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/squirtle").then(res => res.json()).then(data => setPokemon(data)).catch(err => console.log(err))
-  },[])
-  if (!pokemon) {
+    fetchPokemon();
+  }, [])
+
+  if (loading) {
     return <div>Loading .....</div>
   }
+  if (error) {
+    return <div>Error fetching pokemon data
+      <h1>{error.message}</h1>
+    </div>
+  }
+  console.log(pokemon);
+  const filteredPokemon = pokemon.filter((p) => p.name.toLowerCase().includes(searchpokemon.toLowerCase()));
   return (
-    <div className=' flex-col box-border h-96 w-96 p-4 border-7 border-gray-500 flex justify-center rounded-2xl' style={{padding:"10px"}}>
-      {pokemon && (
-        <div>
-          <h2 className='  text-7xl'>{pokemon.name}</h2>
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} style={{ height: "200px" }} />
-        </div>
-      )}
-      <div>
-        <h3 className='text-2xl'>Abilities:</h3>
-        {pokemon.abilities.map((ability) => (
-          <p key={ability.ability.name} className='text-lg'>{ability.ability.name}</p>
-        ))}
+    <div>
+      <h1 className='text-5xl text-center' style={{ margin: "1rem" }}>Let's catch pokemon</h1>
+      <input 
+        type='search' 
+        placeholder="Search pokemon..." 
+        style={{ margin: "1rem", padding: "0.5rem" }}
+        value={searchpokemon}
+        onChange={(e) => setSearchpokemon(e.target.value)}
+      />
+      <div className='flex gap-4 flex-wrap justify-center'>
+        {filteredPokemon && filteredPokemon.map((pokemon) =>
+          <div className='box-border w-96 p-4 border-7 border-gray-500 rounded-2xl' style={{ padding: "10px" }}>
+            <h2 className='text-7xl'>{pokemon.name}</h2>
+            <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} style={{ height: "200px" }} />
+            <p className='text-2xl border-amber-100 border-4 rounded-3xl'>{pokemon.types.map((curr) => curr.type.name).join(", ")}</p>
+            <h3 className='text-2xl'>Abilities:</h3>
+            <div className='grid grid-cols-3'>
+              <span className='mx-2'>Height : {pokemon.height}</span>
+              <span className='mx-2'>Weight : {pokemon.weight}</span>
+              <span className='mx-2'>Speed : {pokemon.stats[5].base_stat}</span>
+              <span className='mx-2'>Experience : {pokemon.base_experience}</span>
+              <span className='mx-2'>Attack : {pokemon.stats[1].base_stat}</span>
+             <span className='mx-2'>Abilities: {pokemon.abilities.map((ability) => ability.ability.name).slice(0, 1).join(", ")}</span>
+            </div>
+          </div>
+        )}
+
       </div>
     </div>
   );
